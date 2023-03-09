cfbf783bd83f4725c2748ab402b628f2f4eb5183
abu jafar <abujafarshiddik123@gmail.com>
2023-03-09 12:02:07 +0600

    adjacency matrix

--- adjacency_matix.cpp
+++ adjacency_matix.cpp
@@ -6,7 +6,7 @@
     0------1
           / \
           2--3
-      
+
     0-1
     1-0
     1-2
@@ -16,44 +16,62 @@
     3-1
     3-2
 
-    0 1 0 0 
-    1 0 1 1 
-    0 1 0 1 
-    0 1 1 0 
+    0 1 0 0
+    1 0 1 1
+    0 1 0 1
+    0 1 1 0
 
     nodes = 4
     time complexity->O(n^2)   A-----B(n number of edges can include each node)
     tc = O(E) = because we just count how many nodes are direct conncted by each edge
     in worst case it can be, E = n^2
-    same for space complexity 
+    same for space complexity
     space complexity->O(n^2)
 */
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int nodes = 4;
-    int matrix[nodes][nodes] = {};
-    matrix[0][1] = 1;
+    // int nodes = 4;
+    // int matrix[nodes][nodes] = {};
+    // matrix[0][1] = 1;
 
-    matrix[1][0] = 1;
-    matrix[1][2] = 1;
-    matrix[1][3] = 1;
+    // matrix[1][0] = 1;
+    // matrix[1][2] = 1;
+    // matrix[1][3] = 1;
 
-    matrix[2][1] = 1;
-    matrix[2][3] = 1;
+    // matrix[2][1] = 1;
+    // matrix[2][3] = 1;
 
-    matrix[3][1] = 1;
-    matrix[3][2] = 1;
+    // matrix[3][1] = 1;
+    // matrix[3][2] = 1;
 
-    for (int i = 0; i < nodes; i++)
+    // for (int i = 0; i < nodes; i++)
+    // {
+    //     for (int j = 0; j < nodes; j++)
+    //     {
+    //         cout<<matrix[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    int n, e;
+    cin >> n >> e;
+    int matrix[n + 1][n + 1] = {};
+
+    for (int i = 1; i <= e; i++)
     {
-        for (int j = 0; j < nodes; j++)
-        {
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
+        int u, v, w;
+        cin >> u >> v >> w;
+        matrix[u][v] = w;
     }
 
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
